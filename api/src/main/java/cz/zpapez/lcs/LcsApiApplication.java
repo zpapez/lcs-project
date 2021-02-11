0792fa5a6aceebefe8aaa19b886a413e8cef5f6b
Zdenek Papez <zdenek.papez@gmail.com>
2021-02-11 23:06:10 +0100

    build.gradle, api controller runs

---
+++ api/src/main/java/cz/zpapez/lcs/LcsApiApplication.java
@@ -0,0 +1,13 @@
+package cz.zpapez.lcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LcsApiApplication.class, args);
+    }
+
+}
---
+++ api/src/main/java/cz/zpapez/lcs/MyService.java
@@ -0,0 +1,17 @@
+package cz.zpapez.lcs;
+
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class MyService {
+
+    public void fetchData() {
+
+        log.info("service running");
+
+    }
+
+}
